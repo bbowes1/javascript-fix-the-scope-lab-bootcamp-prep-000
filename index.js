e5ab50ef194a7fa6ab80a4cae58077a17219af51
Benjamin Bowes <bbowes999@gmail.com>
2017-12-22 15:05:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var animal = 'dog'
+  var animal = 'cat'
 
 function myAnimal() {
-  return animal
-}
+  var animal = 'dog'
+  return animal 
+ }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+ function yourAnimal() {
+  return animal 
+  
+ 
 }
 
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  const two 
 }
 
 var funkyFunction = function() {
